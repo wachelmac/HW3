ef66d01500c9212e3e51005c630bb271e2f83ea4
wachelmac <78569827+wachelmac@users.noreply.github.com>
2021-03-21 15:54:27 -0500

    Update API

--- KdTreeST.java
+++ KdTreeST.java
@@ -1,33 +1,53 @@
-public class KdTreeST<Value> {
+//Rachel McMullan
+//George O'Malley
+//Group 7
 
-    // construct an empty symbol table of points 
-    public KdTreeST()
-
-    // is the symbol table empty? 
-    public boolean isEmpty()
-
-    // number of points
-    public int size()
-
-    // associate the value val with point p
-    public void put(Point2D p, Value val)
-
-    // value associated with point p 
-    public Value get(Point2D p)
-
-    // does the symbol table contain point p? 
-    public boolean contains(Point2D p)
-
-    // all points in the symbol table 
-    public Iterable<Point2D> points()
-
-    // all points that are inside the rectangle (or on the boundary) 
-    public Iterable<Point2D> range(RectHV rect)
-
-    // a nearest neighbor of point p; null if the symbol table is empty 
-    public Point2D nearest(Point2D p)
-
-    // unit testing (required)
-    public static void main(String[] args)
-
+public class KdTreeST {
+    
+        //construct an empty symbol table of points
+    public KdTreeST(){
+        
+    }
+    
+    //return true if symbol table is empty
+    public boolean isEmpty(){
+        return true;
+    }
+    
+    //return number of points 
+    public int size(){
+        return 0;
+    }
+    
+    /*
+    //associate the value val with point p
+    public void put(Point2D p, Value val){
+        
+    }
+    
+    //return value associated with point p
+    public Value get(Point2D p){
+       
+    }
+    */
+    
+    //returns true if the symbol table contains point p
+    public boolean contains(Point2D p){
+        return true;
+    }
+    
+    /*
+    //all points in the symbol table
+    public Iterable<Point2D> points(){
+    }
+    
+    //all points that are inside or on the boundary of the rectangle 
+    public Iterable<Point2D> range(RectHV rect){
+    }
+    */
+    //returns nearest neighbor of point p or null if the symbol table is empty
+    public Point2D nearest (Point2D p) {
+        return p; 
+    }
+    
 }
