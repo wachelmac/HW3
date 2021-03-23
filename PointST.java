4ea056ab2778f1a2f6d1080c1a37d08a530b1036
wachelmac <78569827+wachelmac@users.noreply.github.com>
2021-03-23 16:13:29 -0500

    Consolidation

--- PointST.java
+++ PointST.java
@@ -2,55 +2,66 @@
 //George O'Malley
 //Group 7
 
-public class PointST {
-    // what member variables does this class have??
-	
+public class PointST<Value>{
+    
+    RedBlackBST table=new RedBlackBST();
+    
     //construct an empty symbol table of points
-    public PointST(){
-        
+    public PointST(){  
     }
     
     //return true if symbol table is empty
     public boolean isEmpty(){
-        return true;
+        return table.isEmpty();
     }
     
     //return number of points 
     public int size(){
-        return 0;
+        return table.size();
     }
     
-    /*
     //associate the value val with point p
     public void put(Point2D p, Value val){
-    	if (p == null) throw new IllegalArgumentException("called put() with null argument p");
-    	if (val == null) throw new IllegalArgumentException("called put() with null argument val");
+        table.put(p, val);
     }
     
     //return value associated with point p
-    public Value get(Point2D p){
-    	if (p == null) throw new IllegalArgumentException("called get() with null argument");
-    }
-    */
+    //public Value get(Point2D p){
+       //return table.get(p); //this should not be a problem??
+    //}
     
     //returns true if the symbol table contains point p
     public boolean contains(Point2D p){
-    	if (p == null) throw new IllegalArgumentException("called contains() with null argument");
-        return true;
+        return table.contains(p);
     }
     
-    /*
     //all points in the symbol table
     public Iterable<Point2D> points(){
+        return table.keys();
     }
     
     //all points that are inside or on the boundary of the rectangle 
     public Iterable<Point2D> range(RectHV rect){
+        double xmin=rect.xmin();
+        double xmax=rect.xmax();
+        double ymin=rect.ymin();
+        double ymax=rect.ymax();
+        
+        
+        Point2D bottomLeft=new Point2D(xmin, ymin);
+        //Point2D bottomRight=new Point2D(xmax, ymin);
+        //Point2D topLeft=new Point2D(xmin, ymax);
+        Point2D topRight=new Point2D(xmax,ymax);
+        
+        
+        table.keys(bottomLeft, topRight);
+        
+        
+        return range(rect);
     }
-    */
-    //returns nearest neighbor of point p or null if the symbol table is empty
+    
+    //turns nearest neighbor of point p or null if the symbol table is empty
     public Point2D nearest (Point2D p) {
-    	if (p == null) throw new IllegalArgumentException("called nearest() with null argument");
         return p; 
     }
 }
